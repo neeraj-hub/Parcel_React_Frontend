62655758984952207117bb813bbc067266d3d902
neeraj-hub <neerajs057@gmail.com>
2025-04-22 00:41:42 +0530

    removed ignored folders from tracking

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import Button from "./components/Button-Inline/Button";
 
 const App = () => {
     return (
-        <>
-            <h1>Hello, React! This is a App component which is of no use but test...Why not button ?</h1>
-            <h2>Parcel React App</h2>
-            <Button />
-        </>
+      <>
+        <h1>
+          Hello, React! This is a App component which is of no use but
+          test...Why not button ?
+        </h1>
+        <h2>Parcel React App</h2>
+        <h3>This is a bit smaller text</h3>
+        <Button />
+      </>
     );
 };
 
