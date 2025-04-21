c9bf10ba95727f18adf9d4660a0044dc85b23730
neeraj-hub <neerajs057@gmail.com>
2025-04-22 00:28:29 +0530

    parcel setup with react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button-Inline/Button";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello, React! This is a App component which is of no use but test...Why not button ?</h1>
+            <h2>Parcel React App</h2>
+            <Button />
+        </>
+    );
+};
+
+export default App;
