5480cc3d5ce0b08998a29bfd97bb739d508b94be
neeraj-hub <neerajs057@gmail.com>
2025-04-22 12:43:42 +0530

    removing unwanted folders

--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './App';
-
-createRoot(document.getElementById("root")).render(<App />); // createRoot(container!) if you use TypeScript
